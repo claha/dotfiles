8c8be5c6966957a7a68f978cc075b68d5e447bec
Claes Hallström <hallstrom.claes@gmail.com>
2021-03-26 22:25:38 +0100

    Hide emacs gui elements in early-init instead
    This seems to make emacs start up a bit faster.

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,4 +1,4 @@
-;; Initialise installed packages
+;; Initialise installed packages.
 (setq package-enable-at-startup t)
 
 ;; Allow loading from the package cache.
@@ -6,3 +6,11 @@
 
 ;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize t)
+
+;; Hide GUI elements early.
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(setq inhibit-startup-screen t)
+(setq use-file-dialog nil)
