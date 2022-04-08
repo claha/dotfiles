e79bc6bab7d69814a323a4d186c46f4683d0505b
Claes Hallström <hallstrom.claes@gmail.com>
2022-04-08 11:08:30 +0200

    Simplify emacs startup hook

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -4,11 +4,7 @@
 ;; Profile emacs startup
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (message "Emacs loaded in %s with %d garbage collections."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time)))
-                     gcs-done)))
+            (message "Emacs loaded in %s." (emacs-init-time))))
 
 ;; Load configuration
 (let* ((file (expand-file-name "conf.org" user-emacs-directory))
