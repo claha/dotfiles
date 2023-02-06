5b4d7421b487954540b55e0860cb74bb2a2f90a5
Claes Hallström <hallstrom.claes@gmail.com>
2023-02-06 10:02:58 +0100

    Upgrade pip in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 python -m venv /tmp/venv --prompt ansible
 # shellcheck source=/dev/null
 source /tmp/venv/bin/activate
-pip install ansible==7.1.0
+pip install --upgrade pip ansible==7.1.0
 
 ansible-playbook main.yaml
