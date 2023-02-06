b201001f502b5b0998fd7ae9b049f7c2a6bed8b2
Claes Hallström <hallstrom.claes@gmail.com>
2023-02-06 10:04:04 +0100

    Silent pip in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 python -m venv /tmp/venv --prompt ansible
 # shellcheck source=/dev/null
 source /tmp/venv/bin/activate
-pip install --upgrade pip ansible==7.1.0
+pip install --upgrade pip ansible==7.1.0 --quiet
 
 ansible-playbook main.yaml
