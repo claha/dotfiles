4ca4d14fa6a4a2f1a6e3eb873df73e23b9f12636
Claes Hallström <hallstrom.claes@gmail.com>
2023-06-11 21:19:29 +0200

    Update ansible to version 8.0.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 python -m venv /tmp/venv --prompt ansible
 # shellcheck source=/dev/null
 source /tmp/venv/bin/activate
-pip install --upgrade pip ansible==7.2.0 --quiet
+pip install --upgrade pip ansible==8.0.0 --quiet
 
 ansible-playbook main.yaml
