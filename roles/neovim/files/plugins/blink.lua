e08ca189cb10636e03fd40947958c47dd64ccff9
Claes Hallström <hallstrom.claes@gmail.com>
2025-03-15 20:51:05 +0100

    nvim: add blink.cmp

---
+++ roles/neovim/files/plugins/blink.lua
@@ -0,0 +1,11 @@
+return {
+  "saghen/blink.cmp",
+  dependencies = "rafamadriz/friendly-snippets",
+
+  opts = {
+    keymap = { preset = "default" },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+  },
+}
